5b2ff73936e6dc77ccfc4e42dd3c5f740931f87b
Vivian Rook <91909295+vivian-rook@users.noreply.github.com>
2024-04-01 08:47:12 -0400

    quarry on k8s (#31)
    * Terraform k8s and db
    
    Bug: T349032
    
    * ansible to install cloud provider
    
    Bug: T349032
    
    * livenessProbe
    
    * probes
    
    * removing unused ansible
    
    * update tf credentials
    
    * db nfs to prod
    
    * nfs paths
    
    * secret
    
    * Update database
    
    * New database
    
    * new nfs server
    
    * production token
    
    * Remove banner
    
    * Override default banner

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+if ! command -v kubectl ; then
+  echo "please install kubectl"
+  exit 1
+fi
+
+if ! command -v helm ; then
+  echo "please install helm"
+  exit 1
+fi
+
+if ! command -v tofu; then
+  echo "please install tofu"
+  exit 1
+fi
+
+python3 -m venv .venv/deploy
+source .venv/deploy/bin/activate
+pip install ansible==8.1.0 kubernetes==26.1.0
+
+cd tofu
+tofu init
+tofu apply # -auto-approve
+export KUBECONFIG=$(pwd)/kube.config
+
+cd ../ansible
+ansible-playbook quarry.yaml
+#kubectl create namespace quarry --dry-run=client -o yaml | kubectl apply -f -
+#helm -n quarry upgrade --install quarry helm-quarry -f helm-quarry/prod-env.yaml
+
---
+++ tofu/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  # license is incompatable at version 1.6.0
+  required_version = "= 1.6.2"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.51.0"
+    }
+  }
+}
+
+provider "openstack" {
+  auth_url                      = var.auth-url
+  tenant_id                     = var.tenant_id
+  application_credential_id     = var.application_credential_id
+  application_credential_secret = var.application_credential_secret
+}
